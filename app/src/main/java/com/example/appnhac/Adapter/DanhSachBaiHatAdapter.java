4f6c8a287226f4bbeb030c0a309de5065f9a8443
lethekhoi <lethekhoi@gmail.com>
2020-03-12 21:31:28 +0700

    update luot thich danh sach bai hat

--- app/src/main/java/com/example/appnhac/Adapter/DanhSachBaiHatAdapter.java
+++ app/src/main/java/com/example/appnhac/Adapter/DanhSachBaiHatAdapter.java
@@ -1,19 +1,27 @@
 package com.example.appnhac.Adapter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.appnhac.Model.Baihat;
 import com.example.appnhac.R;
+import com.example.appnhac.Service.APIService;
+import com.example.appnhac.Service.Dataservice;
 
 import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class DanhSachBaiHatAdapter extends RecyclerView.Adapter<DanhSachBaiHatAdapter.ViewHolder> {
     Context context;
@@ -56,7 +64,32 @@ public class DanhSachBaiHatAdapter extends RecyclerView.Adapter<DanhSachBaiHatAd
             txttenbaihat = itemView.findViewById(R.id.txtdanhsachtenbaihat);
             txtcasi = itemView.findViewById(R.id.txtdanhsachtencasi);
             imgluotthich = itemView.findViewById(R.id.imgdanhsachbaihatluotthich);
+            imgluotthich.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    imgluotthich.setImageResource(R.drawable.iconloved);
+                    Dataservice dataservice = APIService.getService();
+                    Call<String> callback = dataservice.UpdateLuotThich("1", mangbaihat.get(getPosition()).getIdBaiHat());
+                    callback.enqueue(new Callback<String>() {
+                        @Override
+                        public void onResponse(Call<String> call, Response<String> response) {
+                            String ketqua = response.body();
+                            if (ketqua.equals("OK")) {
+                                Toast.makeText(context, "Đã thích", Toast.LENGTH_SHORT).show();
 
+                            } else {
+                                Toast.makeText(context, "Bị lỗi", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<String> call, Throwable t) {
+                            Log.d("BBB", "false lt");
+                        }
+                    });
+                    imgluotthich.setEnabled(false);
+                }
+            });
         }
 
 
